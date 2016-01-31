db9b09d21ccf18af64c9781954af97679cf7fe37
anant <ananttibrewal@gmail.com>
2016-01-31 15:58:37 -0600

    first commit

--- src/main/java/HelloHandler.java
+++ src/main/java/HelloHandler.java
@@ -11,11 +11,11 @@ import java.io.IOException;
  */
 public class HelloHandler extends AbstractHandler {
         public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-                throws IOException, ServletException
+                throws IOException
         {
             response.setContentType("text/html;charset=utf-8");
             response.setStatus(HttpServletResponse.SC_OK);
             baseRequest.setHandled(true);
-            response.getWriter().println("<h1>Hello World</h1>");
+            response.getWriter().println("<h1>Hello World</h1>"); //anant
         }
 }
